4de24858c3b5edc2b75bba2ced79d989ecc15225
sakshisb <36100959+sakshisb@users.noreply.github.com>
2023-09-08 19:16:06 +0530

    Add files via upload

---
+++ Project 1/script.js
@@ -0,0 +1,15 @@
+const panel = document.querySelectorAll('.panel')
+
+panel.forEach((panel) => {
+       panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+       })
+})
+
+function removeActiveClasses() {
+    panel.forEach((panel) => {
+        panel.classList.remove('active')
+
+    }) 
+}
